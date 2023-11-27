0fcbbc46fc16a1aea0a3a186f20dcb1a40d2fa5c
Kevin Xiao <xlzqwerty1@gmail.com>
2023-11-27 14:57:14 -0800

    Update macos zsh config

--- zsh-config/macos.zsh
+++ zsh-config/macos.zsh
@@ -1,5 +1,13 @@
 ## MacOS config
+
+# Install fzf via homebrew and run the install command for completions & keybindings
 source "$HOME/.fzf.zsh"
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f "$HOME/src/google-cloud-sdk/path.zsh.inc" ]; then . "$HOME/src/google-cloud-sdk/path.zsh.inc"; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f "$HOME/src/google-cloud-sdk/completion.zsh.inc" ]; then . "$HOME/src/google-cloud-sdk/completion.zsh.inc"; fi
 
 # Keybinds (macOS -- wezterm)
 bindkey '^[[A' history-substring-search-up   # UP
