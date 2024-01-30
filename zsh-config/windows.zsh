4873059de8ee045ff53e2318d714c841c74901ce
Kevin Xiao <xlzqwerty1@gmail.com>
2024-01-30 01:21:54 -0800

    [zsh] Add av1-search and av1-encode functions

--- zsh-config/windows.zsh
+++ zsh-config/windows.zsh
@@ -26,6 +26,28 @@ function unzipjis {
   7z.exe x $1 -mcp=932
 }
 
+function av1-search {
+  ab-av1 crf-search --encoder av1_nvenc --preset p7 --pix-format yuv420p10le -i $1
+}
+
+# Encode video to AV1 using NVENC
+function av1-encode {
+  zmodload zsh/zutil
+  zparseopts -D -F -K -- \
+    {i,-input}:=arg_input \
+    {o,-output}:=arg_output \
+    {q,-quality}:=arg_cq || return 1
+  if [[ -z $arg_input || -z $arg_cq ]]; then
+    echo "Usage: av1-encode -i <input> -o <output> -q <cq>"
+    return 1
+  fi
+  if [[ -z $arg_output ]]; then
+    arg_output=$arg_input:r.av1.mkv
+  fi
+  echo ffmpeg -i $arg_input[-1] -c:v av1_nvenc -pix_fmt p010le -preset p7 -tune hq -highbitdepth true -cq:v $arg_cq[-1] -c:a libopus -b:a 128k $arg_output[-1]
+  ffmpeg -i $arg_input[-1] -c:v av1_nvenc -pix_fmt p010le -preset p7 -tune hq -highbitdepth true -cq:v $arg_cq[-1] -c:a libopus -b:a 128k $arg_output[-1]
+}
+
 # Keybinds (Windows -- wezterm)
 bindkey '^[[A' history-substring-search-up   # UP
 bindkey '^[[B' history-substring-search-down # DOWN
