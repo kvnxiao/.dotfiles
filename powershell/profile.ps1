e50d590334d35e7c5a33b05915360d1207de4092
Kevin Xiao <xlzqwerty1@gmail.com>
2026-01-06 18:03:17 -0800

    [powershell] Add Claude env vars

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -47,6 +47,11 @@ fnm env --use-on-cd | Out-String | Invoke-Expression
 $env:FZF_DEFAULT_OPTS="--color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796,fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6,marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796"
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+u' -PSReadlineChordReverseHistory 'Ctrl+r'
 
+# Claude Code
+$env:ENABLE_LSP_TOOL=1
+$env:ENABLE_EXPERIMENTAL_MCP_CLI=true
+$env:ANTHROPIC_MODEL="claude-opus-4-5-20251101"
+
 Invoke-Expression (&starship init powershell)
 Invoke-Expression (& { (atuin init powershell --disable-up-arrow | Out-String) })
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
