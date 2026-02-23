4ad410435add35803da7d60e0191091dfbe65905
Kevin Xiao <xlzqwerty1@gmail.com>
2026-02-22 23:05:43 -0500

    [fish] replace sed with string for _cached_eval_post_atuin

--- fish/functions/hooks/_cached_eval_post_atuin.fish
+++ fish/functions/hooks/_cached_eval_post_atuin.fish
@@ -1,6 +1,22 @@
 function _cached_eval_post_atuin --description "Post-process atuin init cache"
   set -l file $argv[1]
 
-  # Remove eager ATUIN_SESSION init (deferred via function wrappers in config.fish)
-  sed -i '/^if not set -q ATUIN_SESSION/,/^end$/d' "$file"
+  # Remove eager ATUIN_SESSION init block (deferred via function wrappers in config.fish)
+  # Block is exactly:
+  #   if not set -q ATUIN_SESSION; or test "$ATUIN_SHLVL" != "$SHLVL"
+  #       set -gx ATUIN_SESSION (atuin uuid)
+  #       set -gx ATUIN_SHLVL $SHLVL
+  #   end
+  set -l skip false
+  set -l lines
+  while read -l line
+    if string match -q 'if not set -q ATUIN_SESSION*' -- $line
+      set skip true
+    else if test "$skip" = true; and string match -qr '^\s*end\s*$' -- $line
+      set skip false
+    else if test "$skip" = false
+      set -a lines $line
+    end
+  end <"$file"
+  printf '%s\n' $lines >"$file"
 end
