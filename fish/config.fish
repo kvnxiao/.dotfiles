a545ac262ca6e6534052b21a3e12246dcffbb270
Kevin Xiao <xlzqwerty1@gmail.com>
2026-05-14 22:21:23 -0400

    [fish] Remove forced claude code effort level

--- fish/config.fish
+++ fish/config.fish
@@ -59,5 +59,5 @@ if status is-interactive
   set -gx ANTHROPIC_MODEL "opus[1m]"
   set -gx CLAUDE_CODE_ENABLE_TASKS true
   set -gx CLAUDE_CODE_EXPERIMENTAL_AGENT_TEAMS 1
-  set -gx CLAUDE_CODE_EFFORT_LEVEL "max"
+  # set -gx CLAUDE_CODE_EFFORT_LEVEL "max"
 end
