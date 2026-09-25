628f6337e7ff48cd9dc8b7d05f49fa9be4a3e0a0
Kevin Xiao <xlzqwerty1@gmail.com>
2026-09-25 17:20:39 -0400

    Remove terra abbr

--- fish/conf.d/abbr-codex.fish
+++ fish/conf.d/abbr-codex.fish
@@ -3,7 +3,6 @@ if status is-interactive
 
   set -l ASTRA_MODEL gpt-6-astra
   set -l SOL_MODEL gpt-6-sol
-  set -l TERRA_MODEL gpt-5.6-terra
   set -l LUNA_MODEL gpt-6-luna
   set -l EFFORT_NAMES '' high med low
   set -l EFFORT_VALUES xhigh high medium low
@@ -13,7 +12,6 @@ if status is-interactive
     set -e EFFORT_VALUES[1]
     abbr -a "astra$effort_name" "codex --model=\"$ASTRA_MODEL\" --config model_reasoning_effort=\"$effort_value\""
     abbr -a "sol$effort_name" "codex --model=\"$SOL_MODEL\" --config model_reasoning_effort=\"$effort_value\""
-    abbr -a "terra$effort_name" "codex --model=\"$TERRA_MODEL\" --config model_reasoning_effort=\"$effort_value\""
     abbr -a "luna$effort_name" "codex --model=\"$LUNA_MODEL\" --config model_reasoning_effort=\"$effort_value\""
   end
 end
